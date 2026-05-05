72392375192ccc6e30098a41e04338c41fa5a553
filipdzivy@proton.me <filip@telekom.ip>
2026-05-05 21:24:18 +0200

    fixed hardcoded env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   apiUrl: '',
   keycloak: {
-    url: 'http://keycloak:74-234-72-21.sslip.io',
+    url: 'https://keycloak.74-234-72-21.sslip.io',
     realm: 'ISK',
     clientId: 'isk-client',
   },
